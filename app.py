80c74fad5c11bf66e04cc4c969260ea85f409a27
rakib2067 <rakib7@hotmail.co.uk>
2022-08-27 17:51:36 +0100

    Basic Calculator working

--- app.py
+++ app.py
@@ -1,1 +1,27 @@
-print("Hello world")
+def main():
+    print("Welcome to the calculator app!")
+    while True:
+        calculation = input("Enter your calculation:")
+        print(f'calulation is ' + calculation)
+        splitted = calculation.split()
+        print(splitted)
+        input1 = int(splitted[0])
+        input2 = int(splitted[2])
+        if splitted[1] == "x":
+            answer = input1 * input2
+        elif splitted[1] == "+":
+            answer = input1 + input2
+        elif splitted[1] == "-":
+            answer = input1 - input2
+        elif splitted[1] == "/":
+            answer = input1 / input2
+        else:
+            print("invalid operator")
+
+        print("answer is: " + str(answer))
+        resume = input("would you like to continue? Y or N")
+        if resume == "N":
+            break
+
+
+main()
